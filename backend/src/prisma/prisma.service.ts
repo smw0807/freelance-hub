6f0dfdbfa51ed14e080a80bcd45a8a08c77bdaf1
smw0807 <smw0807@gmail.com>
2026-03-05 19:30:30 +0900

    fix: prisma 연결안되는 문제 수정

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  constructor(private configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.get<string>('DATABASE_URL'),
+    });
+    super({
+      adapter,
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
